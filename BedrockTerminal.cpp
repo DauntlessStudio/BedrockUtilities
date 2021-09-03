2638856765b6a5bdb6c4fb60f7584e42246ce112
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2021-09-03 12:57:18 -0500

    Android Filesystem Alternative

--- BedrockTerminal.cpp
+++ BedrockTerminal.cpp
@@ -1,22 +1,13 @@
 // BedrockTerminal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+#pragma once
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <string>
 #include <map>
 #include "getopt.h"
-#define DATAFILE "directory.dat"
+#include "DirectoryHandler.h"
 
 using namespace std;
-
-struct UserData
-{
-	std::string resource_path;
-	std::string behavior_path;
-
-	UserData(std::string resource_path_in = "", std::string behavior_path_in = "") : resource_path(resource_path_in), behavior_path(behavior_path_in) {}
-};
 
 enum CommandList
 {
@@ -26,74 +17,11 @@ enum CommandList
 
 static std::map<string, CommandList> mapCommandList;
 
-UserData user_data;
-
-void write_user_data()
-{
-	std::ofstream fout(DATAFILE);
-	fout << user_data.resource_path << ' ' << user_data.behavior_path;
-	fout.close();
-}
-
-void read_user_data()
-{
-	std::ifstream fin(DATAFILE);
-	fin >> user_data.resource_path >> user_data.behavior_path;
-	fin.close();
-}
 
 void ShowUsage()
 {
 	//TODO modify function to show usage examples for a passed command name
 	std::cout << "Try 'bed -h' for more information" << std::endl;
-}
-
-void write_resource_dir(bool bUseSource, string arg)
-{
-	std::string path;
-	if (bUseSource)
-	{
-		path = std::filesystem::current_path().string();
-	}
-	else
-	{
-		if (arg.empty())
-		{
-			ShowUsage();
-			return;
-		}
-		path = arg;
-	}
-
-	user_data.resource_path = path;
-	write_user_data();
-
-	std::cout << "Resource Directory: " << path << std::endl;
-	return;
-}
-
-void write_behavior_dir(bool bUseSource, string arg)
-{
-	std::string path;
-	if (bUseSource)
-	{
-		path = std::filesystem::current_path().string();
-	}
-	else
-	{
-		if (arg.empty())
-		{
-			ShowUsage();
-			return;
-		}
-		path = arg;
-	}
-
-	user_data.behavior_path = path;
-	write_user_data();
-
-	std::cout << "Behavior Directory: " << path << std::endl;
-	return;
 }
 
 void init_command_list()
@@ -110,9 +38,10 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	char* dirArg = NULL;
+	string dirArg;
 	int opt;
 	bool bUseSource = true;
+
 	while ((opt = getopt(argc, argv, "d:s")) != -1)
 	{
 		switch (opt)
---
+++ DirectoryHandler.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits.h>
+#define DATAFILE "directory.dat"
+
+using namespace std;
+
+struct UserData
+{
+	std::string resource_path;
+	std::string behavior_path;
+
+	UserData(std::string resource_path_in = "", std::string behavior_path_in = "") : resource_path(resource_path_in), behavior_path(behavior_path_in) {}
+};
+
+UserData user_data;
+
+void write_user_data()
+{
+	std::ofstream fout(DATAFILE);
+	fout << user_data.resource_path << ' ' << user_data.behavior_path;
+	fout.close();
+}
+
+void read_user_data()
+{
+	std::ifstream fin(DATAFILE);
+	fin >> user_data.resource_path >> user_data.behavior_path;
+	fin.close();
+}
+
+#ifdef _WIN32
+#include <filesystem>
+int write_resource_dir(bool bUseSource, string arg)
+{
+	cout << "Write Path" << endl;
+	std::string path;
+	if (bUseSource)
+	{
+		path = std::filesystem::current_path().string();
+	}
+	else
+	{
+		if (arg.empty())
+		{
+			return 0;
+		}
+		path = arg;
+	}
+
+	user_data.resource_path = path;
+	write_user_data();
+
+	std::cout << "Resource Directory: " << path << std::endl;
+	return 1;
+}
+
+int write_behavior_dir(bool bUseSource, string arg)
+{
+	std::string path;
+	if (bUseSource)
+	{
+		path = std::filesystem::current_path().string();
+	}
+	else
+	{
+		if (arg.empty())
+		{
+			return 0;
+		}
+		path = arg;
+	}
+
+	user_data.behavior_path = path;
+	write_user_data();
+
+	std::cout << "Behavior Directory: " << path << std::endl;
+	return 1;
+}
+#else
+#include <unistd.h>
+int write_resource_dir(bool bUseSource, string arg)
+{
+	std::string path;
+	if (bUseSource)
+	{
+		char buffer[_MAX_PATH]
+		getcwd(buffer, sizeof(buffer) != NULL);
+		path = buffer;
+	}
+	else
+	{
+		if (arg.empty())
+		{
+			return 0;
+		}
+		path = arg;
+	}
+
+	user_data.resource_path = path;
+	write_user_data();
+
+	std::cout << "Resource Directory: " << path << std::endl;
+	return 1;
+}
+
+int write_behavior_dir(bool bUseSource, string arg)
+{
+	std::string path;
+	if (bUseSource)
+	{
+		char buffer[_MAX_PATH]
+		getcwd(buffer, sizeof(buffer) != NULL);
+		path = buffer;
+	}
+	else
+	{
+		if (arg.empty())
+		{
+			return 0;
+		}
+		path = arg;
+	}
+
+	user_data.behavior_path = path;
+	write_user_data();
+
+	std::cout << "Behavior Directory: " << path << std::endl;
+	return 1;
+}
+#endif // _WIN32
